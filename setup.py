293d92b51c1ce60536ff138d28759ad1c99ca6bb
Varun Gandhi <vgandhi@g.harvard.edu>
2023-11-02 13:38:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup
 setup(name='PySNARK',
       version='0.3.1',
       description='Python zk-SNARK execution environment',
-      author='Meilof Veeningen',
-      author_email='meilof@gmail.com',
-      url='https://github.com/meilof/pysnark',
+      author='Varun Gandhi',
+      author_email='vgandhi@g.harvard.edu',
+      url='https://github.com/vargandhi/pysnark',
       packages=['pysnark','pysnark.qaptools','pysnark.libsnark', 'pysnark.zkinterface'],
       package_data={'pysnark.qaptools': ['*.sol']},
       extras_require={
